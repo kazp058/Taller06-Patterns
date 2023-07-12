e111296641a6fd9c67777a7ae7846f72654339e4
kazp058 <kazp058@gmail.com>
2023-07-12 16:40:55 -0500

    Code init

---
+++ Solucion/Taller06_Patrones/src/main/java/edu/espol/taller06_patrones/Taller06_Patrones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.espol.taller06_patrones;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller06_Patrones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
